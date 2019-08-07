e64097110bcd8f344f81e85f3859748d32ae7b41
Erin Ponsonby <erinponsonby@gmail.com>
2019-08-07 14:21:46 -0400

    Create new trip

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -6,4 +6,36 @@ class TripsController < ApplicationController
         erb :'trips/index'
     end
 
+    get '/trips/new' do
+        authenticate
+        erb :'trips/new'
+    end
+
+    post '/trips' do
+        if params[:country].empty?
+            redirect to '/trips/new'
+          else
+            trip = Trip.create(content: params[:content])
+          end
+          current_user.trips << trip
+          current_user.save
+          redirect to '/users/:user_id'
+
+
+    get '/trips/:trip_id' do
+        @trip = Trip.find_by(id: params[:trip_id])
+        erb :'trips/show_trip'
+    end
+
+    delete '/trips/:trip_id' do
+        @trip = Trip.find_by(id: params[:trip_id])
+        if logged_in? && current_user.trips.include?(@trip)
+          @trip.destroy
+        else
+          redirect '/login'
+        end
+    end
+    
+
+
 end
