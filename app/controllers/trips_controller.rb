0489e3250f9aab75f3e09bcdd17cd9222b28d78f
Erin Ponsonby <erinponsonby@gmail.com>
2019-08-08 11:28:19 -0400

    Add CRUD functionality to highlights

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -22,23 +22,57 @@ class TripsController < ApplicationController
         trip.highlights << highlight
         current_user.trips << trip
         current_user.save
-        redirect to "/#{:user_id}/trips"
+        redirect to "/#{current_user.id}/trips"
+    end
+
+    get '/trips/:trip_id/highlights/new' do
+        authenticate
+        @trip = Trip.find_by(id: params[:trip_id])
+        erb :'/highlights/new'
+    end
+
+    post '/highlights' do
+        highlight = Highlight.create(highlight_category: params[:highlight_category], place: params[:place], notes: params[:notes], trip_id: params[:trip_id])
+        trip = Trip.find_by(id: params[:trip_id])
+        trip.highlights << highlight
+        redirect to "/trips/#{trip.id}"
     end
 
     get '/trips/:trip_id' do
         @trip = Trip.find_by(id: params[:trip_id])
+        @highlights = @trip.highlights
         erb :'trips/show_trip'
     end
 
-    get '/trips/:trip_id/edit'
+    get '/trips/:trip_id/edit' do
+        @trip = Trip.find_by(id: params[:trip_id])
+        erb :'trips/edit'
+    end
+
+    get '/trips/:trip_id/:highlight_id/edit' do
+        @trip = Trip.find_by(id: params[:trip_id])
+        @highlight = Highlight.find_by(id: params[:highlight_id])
+        erb :'highlights/edit'
+    end
+
+    delete '/trips/:trip_id/:highlight_id' do
+        @highlight = Highlight.find_by(id: params[:highlight_id])
+        @trip = Trip.find_by(id: params[:trip_id])
+        if logged_in? && @trip.highlights.include?(@highlight)
+            @highlight.destroy
+            redirect "/trips/#{@trip.id}"
+        else
+            redirect "/trips/#{@trip.id}"
+        end
+    end
 
     delete '/trips/:trip_id' do
         @trip = Trip.find_by(id: params[:trip_id])
         if logged_in? && current_user.trips.include?(@trip)
           @trip.destroy
-          redirect "/#{:user_id}/trips"
+          redirect "/#{current_user.id}/trips"
         else
-          redirect "/#{:user_id}/trips"
+          redirect "/#{current_user.id}/trips"
         end
     end
     
